0f3295aab85ca9ce4b4282567c34ccc1a7b34a62
pgorun <pgorun@ya.ru>
2022-07-19 20:31:36 +0300

    add arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,6 +3,9 @@ const { Command } = require('commander');
 const program = new Command();
 
 program.description('Compares two configuration files and shows a difference.')
-    .option('-V,--version', 'output the version number');
+    .option('-V,--version', 'output the version number')
+    .option('-f,--format <type>', 'output format')
+    .argument('<filepath1>')
+    .argument('<filepath2>');
 
 program.parse();
